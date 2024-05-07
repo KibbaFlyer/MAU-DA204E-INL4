63f0f5d9588f2dd7187bb21dbf3491150dbe7dc9
KibbaFlyer <84014093+KibbaFlyer@users.noreply.github.com>
2024-05-07 09:41:02 +0200

    Adding missing picture on popup

--- MAU-DA204E-INL4/AB-APU-Recipe-Book/obj/Debug/net6.0-windows/App.g.cs
+++ MAU-DA204E-INL4/AB-APU-Recipe-Book/obj/Debug/net6.0-windows/App.g.cs
@@ -46,7 +46,7 @@ namespace AB_APU_Recipe_Book {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -61,7 +61,7 @@ namespace AB_APU_Recipe_Book {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
         public static void Main() {
             AB_APU_Recipe_Book.App app = new AB_APU_Recipe_Book.App();
             app.InitializeComponent();
